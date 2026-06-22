bbbe3abfcb5badc165ca04beba0e1beabc67ca44
banavathsrikanth7 <srikanth10.24@kgpian.iitkgp.ac.in>
2026-06-23 01:17:00 +0530

    firebase env config

--- nami1/backend/app/firebase_config.py
+++ nami1/backend/app/firebase_config.py
@@ -1,8 +1,12 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate(
-    "firebase/serviceAccountKey.json"
+firebase_json = json.loads(
+    os.environ["FIREBASE_CREDENTIALS"]
 )
+
+cred = credentials.Certificate(firebase_json)
 
 firebase_admin.initialize_app(cred)
